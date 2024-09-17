a12a4db10b0c6b9f94759460a706e1cfb18fc95a
Jasark0 <jasonyen2004@gmail.com>
2024-09-17 11:56:04 -0500

    Updated remaining balls tracking

--- js/straight-pool.js
+++ js/straight-pool.js
@@ -1,70 +1,90 @@
 let player1Score = Number(document.querySelector('.player1-score').innerHTML);
-      let player2Score = Number(document.querySelector('.player2-score').innerHTML);
-      const player1Name = localStorage.getItem('player1-name');
-      const player2Name = localStorage.getItem('player2-name');
+let player2Score = Number(document.querySelector('.player2-score').innerHTML);
+const player1Name = localStorage.getItem('player1-name');
+const player2Name = localStorage.getItem('player2-name');
 
-      if (player1Name === null){
-        player1Name = 'Player 1';
-      }
-      if (player2Name === null){
-        player2Name = 'Player 2';
-      }
+if (player1Name === null){
+  player1Name = 'Player 1';
+}
+if (player2Name === null){
+  player2Name = 'Player 2';
+}
 
-      window.onload = function(){
-        if (player1Name){
-          document.getElementById('player1').textContent = player1Name;
-        }
-        if (player2Name){
-          document.getElementById('player2').textContent = player2Name;
-        }
-      }
+window.onload = function(){
+  if (player1Name){
+    document.getElementById('player1').textContent = player1Name;
+  }
+  if (player2Name){
+    document.getElementById('player2').textContent = player2Name;
+  }
+}
 
-      function incrementPlayerOne(){
-        player1Score++;
-        document.querySelector('.player1-score').innerHTML = player1Score;
-      }
+function calRemainingBalls(potted){
+  let remainingBalls = document.querySelector('.remaining-balls').innerHTML - potted;
+  if (remainingBalls === 0){
+    document.querySelector('.remaining-balls').innerHTML = 14;
+  }
+  else{
+    document.querySelector('.remaining-balls').innerHTML = remainingBalls;
+  }
+}
 
-      function increment2PlayerOne(){
-        player1Score+=2;
-        document.querySelector('.player1-score').innerHTML = player1Score;
-      }
-      
-      function increment5PlayerOne(){
-        player1Score+=5;
-        document.querySelector('.player1-score').innerHTML = player1Score;
-      }
+function incrementPlayerOne(){
+  player1Score++;
+  document.querySelector('.player1-score').innerHTML = player1Score;
+  calRemainingBalls(1);
+}
 
-      function increment14PlayerOne(){
-        player1Score+=14;
-        document.querySelector('.player1-score').innerHTML = player1Score;
-      }
+function increment2PlayerOne(){
+  player1Score+=2;
+  document.querySelector('.player1-score').innerHTML = player1Score;
+  calRemainingBalls(2);
+}
 
-      function decrementPlayerOne(){
-        player1Score--;
-        document.querySelector('.player1-score').innerHTML = player1Score;
-      }
+function increment5PlayerOne(){
+  player1Score+=5;
+  document.querySelector('.player1-score').innerHTML = player1Score;
+  calRemainingBalls(5);
+}
 
-      function incrementPlayerTwo(){
-        player2Score++;
-        document.querySelector('.player2-score').innerHTML = player2Score;
-      }
+function increment14PlayerOne(){
+  player1Score+=14;
+  document.querySelector('.player1-score').innerHTML = player1Score;
+  calRemainingBalls(14);
+}
 
-      function increment2PlayerTwo(){
-        player2Score+=2;
-        document.querySelector('.player2-score').innerHTML = player2Score;
-      }
+function decrementPlayerOne(){
+  player1Score--;
+  document.querySelector('.player1-score').innerHTML = player1Score;
+  calRemainingBalls(-1);
+}
 
-      function increment5PlayerTwo(){
-        player2Score+=5;
-        document.querySelector('.player2-score').innerHTML = player2Score;
-      }
+function incrementPlayerTwo(){
+  player2Score++;
+  document.querySelector('.player2-score').innerHTML = player2Score;
+  calRemainingBalls(1);
+}
 
-      function increment14PlayerTwo(){
-        player2Score+=14;
-        document.querySelector('.player2-score').innerHTML = player2Score;
-      }
+function increment2PlayerTwo(){
+  player2Score+=2;
+  document.querySelector('.player2-score').innerHTML = player2Score;
+  calRemainingBalls(2);
+}
 
-      function decrementPlayerTwo(){
-        player2Score--;
-        document.querySelector('.player2-score').innerHTML = player2Score;
-      }
+function increment5PlayerTwo(){
+  player2Score+=5;
+  document.querySelector('.player2-score').innerHTML = player2Score;
+  calRemainingBalls(5);
+}
+
+function increment14PlayerTwo(){
+  player2Score+=14;
+  document.querySelector('.player2-score').innerHTML = player2Score;
+  calRemainingBalls(14);
+}
+
+function decrementPlayerTwo(){
+  player2Score--;
+  document.querySelector('.player2-score').innerHTML = player2Score;
+  calRemainingBalls(-1);
+}
