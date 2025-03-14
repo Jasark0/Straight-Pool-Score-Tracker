31dae840eaaaf2a4a5e4a846a622648e0af034e0
Jason Yen <jasonyen2004@gmail.com>
2025-03-13 21:13:01 -0500

    feat: added rack number, fixed high run.

--- js/straight-pool.js
+++ js/straight-pool.js
@@ -1,6 +1,8 @@
 let player1Score = Number(document.querySelector('.player1-score').innerHTML);
 let player2Score = Number(document.querySelector('.player2-score').innerHTML);
 let remainingBalls = Number(document.querySelector('.remaining-balls').innerHTML);
+let rackNum = 1;
+
 player1Name = localStorage.getItem('player1-name');
 player2Name = localStorage.getItem('player2-name');
 
@@ -37,6 +39,9 @@ function calRemainingBalls(potted){
   else if (remainingBalls === 1){
     remainingBalls = 15;
     document.querySelector('.remaining-balls').innerHTML = remainingBalls;
+    
+    rackNum++;
+    document.querySelector('.rack-number').innerHTML = `(Rack ${rackNum})`;
   }
   else{
     document.querySelector('.remaining-balls').innerHTML = remainingBalls;
@@ -50,7 +55,7 @@ function incrementPlayerOne(){
       document.querySelector('.player1-owe').innerHTML = `Player Owes 1 Ball`;
     }
     else{
-      document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Ball`;
+      document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Balls`;
     }
   }
   else{
@@ -75,7 +80,6 @@ function increment2PlayerOne(){
 function incrementRestPlayerOne(){
   player1Score+=(remainingBalls-1);
   document.querySelector('.player1-score').innerHTML = player1Score;
-  calRemainingBalls(remainingBalls-1);
   if (player1Owe != 0){
     remainingBalls = player1Owe+1;
     document.querySelector('.remaining-balls').innerHTML = remainingBalls;
@@ -84,6 +88,16 @@ function incrementRestPlayerOne(){
 
     player1Owe = 0;
     document.querySelector('.player1-owe').innerHTML = `Player Owes 0 Balls`;
+  }
+  else{
+    calRemainingBalls(remainingBalls-1);
+  }
+
+  player2HighTemp = 0;
+  player1HighTemp+=(remainingBalls-1);
+  if (player1HighTemp > player1High){
+    player1High = player1HighTemp;
+    document.querySelector('.player1-high-run').innerHTML = `High Run: ${player1High}`;
   }
 }
 
@@ -94,7 +108,7 @@ function decrementPlayerOne(){
       document.querySelector('.player1-owe').innerHTML = `Player Owes 1 Ball`;
     }
     else{
-      document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Ball`;
+      document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Balls`;
     }
   }
 
@@ -137,8 +151,6 @@ function increment2PlayerTwo(){
 function incrementRestPlayerTwo(){
   player2Score+=(remainingBalls-1);
   document.querySelector('.player2-score').innerHTML = player2Score;
-  calRemainingBalls(remainingBalls-1);
-
   if (player2Owe != 0){
     remainingBalls = player2Owe+1;
     document.querySelector('.remaining-balls').innerHTML = remainingBalls;
@@ -147,6 +159,16 @@ function incrementRestPlayerTwo(){
 
     player2Owe = 0;
     document.querySelector('.player2-owe').innerHTML = `Player Owes 0 Balls`;
+  }
+  else{
+    calRemainingBalls(remainingBalls-1);
+  }
+
+  player1HighTemp = 0;
+  player2HighTemp+=(remainingBalls-1);
+  if (player2HighTemp > player2High){
+    player2High = player2HighTemp;
+    document.querySelector('.player2-high-run').innerHTML = `High Run: ${player2High}`;
   }
 }
 
