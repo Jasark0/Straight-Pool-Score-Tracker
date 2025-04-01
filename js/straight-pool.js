5f08daa3b03fffcc59379ebebb508a54468256cc
Jasark0 <jasonyen2004@gmail.com>
2025-03-31 21:27:07 -0500

    feat: major revamp in all cases. Added turn based. CR is now disabled.

--- js/straight-pool.js
+++ js/straight-pool.js
@@ -6,15 +6,19 @@ let player2Score = Number(document.querySelector('.player2-score').innerHTML);
 let remainingBalls = Number(document.querySelector('.remaining-balls').innerHTML);
 let rackNum = 1;
 
-let currRackPlayer1Score = 0;
-let currRackPlayer2Score = 0;
-let player1Owe = 0;
-let player2Owe = 0;
+let player1Owe = 0; //player1 owed balls
+let player2Owe = 0; //player2 owed balls
+let playerSpot = 0; //alert to spot balls
 
-let player1High = 0;
-let player2High = 0;
-let player1HighTemp = 0;
-let player2HighTemp = 0;
+let player1High = 0; //player1 high run
+let player2High = 0; //player2 high run
+let player1HighTemp = 0; //current player1 high run
+let player2HighTemp = 0; //current player2 high run
+
+let player1CFoul = 0; //player1 consecutive fouls: 3 consecutive leads to serious foul
+let player2CFoul = 0; //player2 consecutive fouls: 3 consecutive leads to serious foul
+
+let player1Turn = true; //determines the player turn
 
 if (player1Name === null){
   player1Name = "Player 1";
@@ -23,16 +27,42 @@ if (player2Name === null){
   player2Name = 'Player 2';
 }
 
-window.onload = function(){
+window.onload = function(){ //on load, player names and turn name is identified
   if (player1Name){
     document.getElementById('player1').textContent = player1Name;
+    document.getElementById('player-turn-text').textContent = `${player1Name}'s Turn`;
   }
   if (player2Name){
     document.getElementById('player2').textContent = player2Name;
   }
 }
 
-function calRemainingBalls(potted){
+document.addEventListener("keydown", function(event){ //space to switch turns
+  if (event.code === "Space"){
+    event.preventDefault();
+    if (player1Turn){
+      player1Turn = false;
+      document.getElementById('player-turn-text').textContent = `${player2Name}'s Turn`;
+      document.getElementById('player-turn').src = "/images/rightArrow.png";
+
+      player1HighTemp = 0;
+    }
+    else{
+      player1Turn = true;
+      document.getElementById('player-turn-text').textContent = `${player1Name}'s Turn`;
+      document.getElementById('player-turn').src = "/images/leftArrow.png";
+
+      player2HighTemp = 0;
+    }
+
+    if (playerSpot != 0){
+      alert(`Spot ${playerSpot} balls please!`);
+      playerSpot = 0;
+    }
+  }
+});
+
+function calRemainingBalls(potted){ //calculates the remaining balls currently
   remainingBalls = Number(document.querySelector('.remaining-balls').innerHTML - potted);
   if (remainingBalls > 15){
     remainingBalls = 15;
@@ -44,39 +74,63 @@ function calRemainingBalls(potted){
     rackNum++;
     document.querySelector('.rack-number').innerHTML = `(Rack ${rackNum})`;
 
-    currRackPlayer1Score = 0;
-    currRackPlayer2Score = 0;
+    if (playerSpot != 0){
+      alert(`Spot ${playerSpot} balls please!`);
+      playerSpot = 0;
+    }
   }
   else{
     document.querySelector('.remaining-balls').innerHTML = remainingBalls;
   }
 }
 
-function incrementPlayerOne(){
-  if (player1Owe != 0){
-    currRackPlayer1Score++;
+function incrementOwe(player){
+  if (player === "player1"){
     player1Owe--;
-    if (player1Owe === 1){
-      document.querySelector('.player1-owe').innerHTML = `Player Owes 1 Ball`;
+    playerSpot++;
+    document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Balls`;
+  }
+  else if (player === "player2"){
+    player2Owe--;
+    playerSpot++;
+    document.querySelector('.player1-owe').innerHTML = `Player Owes ${player2Owe} Balls`;
+  }
+}
+
+function incrementPlayerOne(){
+  if (!player1Turn){
+    if (playerSpot != 0){
+      alert(`Spot ${playerSpot} balls please!`);
+      playerSpot = 0;
     }
-    else{
-      document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Balls`;
-    }
+
+    player1Turn = true;
+    document.getElementById('player-turn-text').textContent = `${player1Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/leftArrow.png";
+
+    player2HighTemp = 0;
+  }
+  
+  player1Score++;
+  player1HighTemp++;
+  player1CFoul = 0;
+
+  if (player1Owe != 0){
+    incrementOwe("player1");
+  }
+  else if (player2Owe != 0){
+    incrementOwe("player2");
   }
   else{
-    currRackPlayer1Score++;
     calRemainingBalls(1);
   }
 
-  player1Score++;
-  document.querySelector('.player1-score').innerHTML = player1Score;
-
-  player2HighTemp = 0;
-  player1HighTemp++;
   if (player1HighTemp > player1High){
     player1High = player1HighTemp;
     document.querySelector('.player1-high-run').innerHTML = `High Run: ${player1High}`;
   }
+  
+  document.querySelector('.player1-score').innerHTML = player1Score;
 }
 
 function increment2PlayerOne(){
@@ -85,90 +139,84 @@ function increment2PlayerOne(){
 }
 
 function incrementRestPlayerOne(){
-  player2HighTemp = 0;
-  player1HighTemp+=(remainingBalls-1);
-  if (player1HighTemp > player1High){
-    player1High = player1HighTemp;
-    document.querySelector('.player1-high-run').innerHTML = `High Run: ${player1High}`;
-  }
-
-  player1Score+=(remainingBalls-1);
-  document.querySelector('.player1-score').innerHTML = player1Score;
-  if (player1Owe != 0){
-    remainingBalls = player1Owe+1;
-    document.querySelector('.remaining-balls').innerHTML = remainingBalls;
-
-    alert(`Spot ${player1Owe} balls please!`);
-
-    player1Owe = 0;
-    document.querySelector('.player1-owe').innerHTML = `Player Owes 0 Balls`;
-  }
-  else{
-    calRemainingBalls(remainingBalls-1);
-  }
 }
 
 function decrementPlayerOne(){
   player1Score--;
-  currRackPlayer1Score--;
-  document.querySelector('.player1-score').innerHTML = player1Score;
-  calRemainingBalls(-1);
+  player1CFoul++;
+  player1HighTemp = 0;
+  player2HighTemp = 0;
 
-  if (currRackPlayer1Score <= 0){
+  if (player1CFoul === 3){ //serious foul condition: -15 score, reset rack
+    player1CFoul = 0;
+
+    alert(`A serious foul committed: 3 foul rule. At this time, ${player1Name} must rerack all balls, and proceed to break. Please read rule 4.11 for more information.`);
+    remainingBalls = 15;
+    player1Score -= 15;
+    player1Owe = 0;
+
+    document.querySelector('.remaining-balls').innerHTML = remainingBalls;
+    document.querySelector('.player1-score').innerHTML = player1Score;
+    document.querySelector('.player1-owe').innerHTML = `Player Owes 0 Balls`;
+  }
+  else if (remainingBalls === 15){
     player1Owe++;
-    if (player1Owe === 1){
-      document.querySelector('.player1-owe').innerHTML = `Player Owes 1 Ball`;
-    }
-    else if (player1Owe === 2){
-      document.querySelector('.player1-owe').innerHTML = `Player Owes 2 Balls`;
-    }
-    else if (player1Owe >= 3){
-      alert(`A serious foul committed: 3 foul rule. At this time, ${player1Name} must rerack all balls, and proceed to break. Please read rule 4.11 for more information.`);
-      remainingBalls = 15;
-      player1Score -= 15;
-      player1Owe = 0;
-      currRackPlayer1Score = 0;
-      currRackPlayer2Score = 0;
-      
-      document.querySelector('.remaining-balls').innerHTML = remainingBalls;
-      document.querySelector('.player1-score').innerHTML = player1Score;
-      document.querySelector('.player1-owe').innerHTML = `Player Owes 0 Balls`;
-    }
+    document.querySelector('.player1-owe').innerHTML = `Player Owes ${player1Owe} Balls`;
+    document.querySelector('.player1-score').innerHTML = player1Score;
+
+    player1Turn = false;
+    document.getElementById('player-turn-text').textContent = `${player2Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/rightArrow.png";
+  }
+  else{
+    document.querySelector('.player1-score').innerHTML = player1Score;
+    calRemainingBalls(-1);
+    alert(`Spot 1 ball please!`);
+
+    player1Turn = false;
+    document.getElementById('player-turn-text').textContent = `${player2Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/rightArrow.png";
   }
 }
 
 
 
-
-
-
-
+//player 2 functions
 
 function incrementPlayerTwo(){
+  if (player1Turn){
+    if (playerSpot != 0){
+      alert(`Spot ${playerSpot} balls please!`);
+      playerSpot = 0;
+    }
+
+    player1Turn = false;
+    document.getElementById('player-turn-text').textContent = `${player2Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/rightArrow.png";
+
+    player1HighTemp = 0;
+  }
+  
+  player2Score++;
+  player2HighTemp++;
+  player2CFoul = 0;
+
   if (player2Owe != 0){
-    currRackPlayer2Score++;
-    player2Owe--;
-    if (player2Owe === 1){
-      document.querySelector('.player2-owe').innerHTML = `Player Owes 1 Ball`;
-    }
-    else{
-      document.querySelector('.player2-owe').innerHTML = `Player Owes ${player2Owe} Ball`;
-    }
+    incrementOwe("player2");
+  }
+  else if (player1Owe != 0){
+    incrementOwe("player1");
   }
   else{
-    currRackPlayer2Score++;
     calRemainingBalls(1);
   }
 
-  player2Score++;
-  document.querySelector('.player2-score').innerHTML = player2Score;
-
-  player1HighTemp = 0;
-  player2HighTemp++;
   if (player2HighTemp > player2High){
     player2High = player2HighTemp;
     document.querySelector('.player2-high-run').innerHTML = `High Run: ${player2High}`;
   }
+
+  document.querySelector('.player2-score').innerHTML = player2Score;
 }
 
 function increment2PlayerTwo(){
@@ -178,55 +226,43 @@ function increment2PlayerTwo(){
 
 
 function incrementRestPlayerTwo(){
-  player1HighTemp = 0;
-  player2HighTemp+=(remainingBalls-1);
-  if (player2HighTemp > player2High){
-    player2High = player2HighTemp;
-    document.querySelector('.player2-high-run').innerHTML = `High Run: ${player2High}`;
-  }
 
-  player2Score+=(remainingBalls-1);
-  document.querySelector('.player2-score').innerHTML = player2Score;
-  if (player2Owe != 0){
-    remainingBalls = player2Owe+1;
-    document.querySelector('.remaining-balls').innerHTML = remainingBalls;
-
-    alert(`Spot ${player2Owe} balls please!`);
-
-    player2Owe = 0;
-    document.querySelector('.player2-owe').innerHTML = `Player Owes 0 Balls`;
-  }
-  else{
-    calRemainingBalls(remainingBalls-1);
-  }
 }
 
 function decrementPlayerTwo(){
   player2Score--;
-  currRackPlayer2Score--;
-  document.querySelector('.player2-score').innerHTML = player2Score;
-  calRemainingBalls(-1);
+  player2CFoul++;
+  player1HighTemp = 0;
+  player2HighTemp = 0;
 
-  if (currRackPlayer2Score <= 0){
+  if (player2CFoul === 3){ //serious foul condition: -15 score, reset rack
+    player2CFoul = 0;
+
+    alert(`A serious foul committed: 3 foul rule. At this time, ${player2Name} must rerack all balls, and proceed to break. Please read rule 4.11 for more information.`);
+    remainingBalls = 15;
+    player2Score -= 15;
+    player2Owe = 0;
+
+    document.querySelector('.remaining-balls').innerHTML = remainingBalls;
+    document.querySelector('.player2-score').innerHTML = player1Score;
+    document.querySelector('.player2-owe').innerHTML = `Player Owes 0 Balls`;
+  }
+  else if (remainingBalls === 15){
     player2Owe++;
-    if (player2Owe === 1){
-      document.querySelector('.player2-owe').innerHTML = `Player Owes 1 Ball`;
-    }
-    else if (player2Owe === 2){
-      document.querySelector('.player2-owe').innerHTML = `Player Owes 2 Balls`;
-    }
-    else if (player2Owe >= 3){
-      alert(`A serious foul committed: 3 foul rule. At this time, ${player2Name} must rerack all balls, and proceed to break. Please read rule 4.11 for more information.`);
-      remainingBalls = 15;
-      player2Score -= 15;
-      player2Owe = 0;
-      currRackPlayer1Score = 0;
-      currRackPlayer2Score = 0;
-      
-      document.querySelector('.remaining-balls').innerHTML = remainingBalls;
-      document.querySelector('.player2-score').innerHTML = player2Score;
-      document.querySelector('.player2-owe').innerHTML = `Player Owes 0 Balls`;
-      return;
-    }
+    document.querySelector('.player2-owe').innerHTML = `Player Owes ${player2Owe} Balls`;
+    document.querySelector('.player2-score').innerHTML = player2Score;
+
+    player1Turn = true;
+    document.getElementById('player-turn-text').textContent = `${player1Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/leftArrow.png";
+  }
+  else{
+    document.querySelector('.player2-score').innerHTML = player2Score;
+    calRemainingBalls(-1);
+    alert(`Spot 1 ball please!`);
+
+    player1Turn = true;
+    document.getElementById('player-turn-text').textContent = `${player1Name}'s Turn`;
+    document.getElementById('player-turn').src = "/images/leftArrow.png";
   }
 }
